bdd43d5d73164d199deee0a45ded55d82abb2afb
Rbaxters <158490067+Rbaxters@users.noreply.github.com>
2024-04-12 10:00:52 -0400

    Update Bank.cpp

--- Bank.cpp
+++ Bank.cpp
@@ -1,55 +1,98 @@
 #include <iostream>
 #include <string>
+#include <fstream>
 
 using namespace std;
 
+// Function prototypes
+void get_input(double& input, const string& prompt);
+void get_input(int& input, const string& prompt);
 
-void get_input(double& balanceInput, bool& goodInput);  //gain the first input from the user
-
-void output_instructions(); //print the output instructions string
-
-
-int main()
-{
-    double balanceInput;  //store the input
-
+int main() {
+    double initialBalance, annualInterest, monthlyDeposit, monthlyWithdrawal;
+    int monthsActive;
     bool goodInput = false;
 
-    get_input(balanceInput, goodInput);  //call the get input function
+    // Get initial balance
+    get_input(initialBalance, "Please input the initial balance: ");
 
+    // Get annual interest rate
+    get_input(annualInterest, "Please input the annual interest: ");
+
+    // Get months active
+    get_input(monthsActive, "Please input the number of months: ");
+
+    double balance = initialBalance;
+    double totalWithdrawals = 0;
+    double totalDeposits = 0;
+    double totalMonthlyInterest = 0;
+    double monthlyInterest = annualInterest / 12;
+
+    fstream myFile("savingsdata.txt", ios::out);
+
+    for (int month = 1; month <= monthsActive; ++month) {
+        // Get monthly deposit
+        get_input(monthlyDeposit, "How much have you deposited this month?: ");
+        balance += monthlyDeposit;
+        totalDeposits += monthlyDeposit;
+
+        // Get monthly withdrawal
+        get_input(monthlyWithdrawal, "How much have you withdrawn this month?: ");
+        balance -= monthlyWithdrawal;
+        totalWithdrawals += monthlyWithdrawal;
+
+        // Calculate monthly interest
+        double monthlyInterestAmount = balance * monthlyInterest;
+        balance += monthlyInterestAmount;
+        totalMonthlyInterest += monthlyInterestAmount;
+
+        cout << "Balance after month " << month << ": " << balance << endl;
+    }
+
+    // Write final values to file
+    myFile << "FINAL VALUES\n"
+           << "Balance: " << balance << "\n"
+           << "Total amount of Withdrawals: " << totalWithdrawals << "\n"
+           << "Total amount of Deposits: " << totalDeposits << "\n"
+           << "Total Monthly Interest: " << totalMonthlyInterest << "\n";
+
+    myFile.close();
+    cout << "Final data has been written in savingsdata.txt\n";
+
+    // Read and output data from the file
+    ifstream inputFile("savingsdata.txt");
+    if (inputFile.is_open()) {
+        string line;
+        while (getline(inputFile, line)) {
+            cout << line << endl;
+        }
+        inputFile.close();
+    } else {
+        cerr << "Error opening the file." << endl;
+        return 1;
+    }
 
     return 0;
 }
 
-
-void get_input(double& balanceInput, bool& goodInput)  //gain the first input from the user
-{
-    output_instructions();  //output the instructions to the user
-
-    if (cin >> balanceInput)
-    {
-        cout << "Balance is valid" << endl;
-        goodInput = true;  //valid input
+void get_input(double& input, const string& prompt) {
+    cout << prompt;
+    while (!(cin >> input)) {
+        string str;
+        cin.clear();
+        getline(cin, str);  // Clear input buffer
+        cout << str << " is not a valid input. Please try again: ";
     }
-    else
-    {
-        while( !( cin >> balanceInput ) )
-        {
-            //this will be true if an error occurs. It could be while( cin.fail() )
-            //but then you'd have to add the actual input
-            string str;
-            cin.clear();//you need to clear the flags before input
-            getline( cin, str );//read what was written. Since you probably don't need this, look into cin.ignore()
-            cout << str << " is not a number\n";
-        }
-        cout << "Balance is valid" << endl;
-        goodInput = true;  //valid input
-    }
+    cout << "Input is valid." << endl;
 }
 
-void output_instructions() //print the instructions
-
-{
-    cout << "Please input the initial balance:  " << endl;
-    cout << "" << endl;
+void get_input(int& input, const string& prompt) {
+    cout << prompt;
+    while (!(cin >> input)) {
+        string str;
+        cin.clear();
+        getline(cin, str);  // Clear input buffer
+        cout << str << " is not a valid input. Please try again: ";
+    }
+    cout << "Input is valid." << endl;
 }
